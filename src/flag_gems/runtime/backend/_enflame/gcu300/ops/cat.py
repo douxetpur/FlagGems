5334856ce859b156d0f79e324350a848060682f9
liuxh <liuxh0217@163.com>
2026-08-04 20:02:29 +0800

    add logger debug

--- src/flag_gems/runtime/backend/_enflame/gcu300/ops/cat.py
+++ src/flag_gems/runtime/backend/_enflame/gcu300/ops/cat.py
@@ -323,6 +323,7 @@ def cat_out(
 def cat(
     A: Union[Tuple[torch.Tensor, ...], List[torch.Tensor]], dim: int = 0
 ) -> torch.Tensor:
+    logger.debug("GEMS_ENFLAME CAT")
     A = list(A)
     if _should_use_uint8_view_path(A):
         mode, payload, original_dtype = _cat_build_working_list_uint8_view(A, dim)
